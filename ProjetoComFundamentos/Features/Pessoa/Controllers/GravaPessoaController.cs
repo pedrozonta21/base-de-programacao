aaa901ee46a6833d4d55393acc311ec3373aa1e4
pedrozonta21 <pedrozonta6@gmail.com>
2022-09-07 18:55:20 -0300

    criada a controller de pessoa

---
+++ ProjetoComFundamentos/Features/Pessoa/Controllers/GravaPessoaController.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoComFundamentos.Features.Pessoa.Models;
+
+namespace ProjetoComFundamentos.Features.Pessoa.Controllers;
+
+public partial class PessoaController
+{
+    public PessoaController(/*criar fábrica de service de gravacao pelo tipo da pessoa*/)
+    {
+
+    }
+
+    public void InserirPessoa(PessoaModel pessoaModel) 
+    {
+        /*criar fábrica de service de gravacao pelo tipo da pessoa*/
+    }
+
+    public void AtualizarPessoa(PessoaModel pessoaModel)
+    {
+        /*criar fábrica de service de gravacao pelo tipo da pessoa*/
+    }
+}
---
+++ ProjetoComFundamentos/Features/Pessoa/Controllers/PessoaController.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoComFundamentos.Features.Endereco.Services.Interfaces;
+using ProjetoComFundamentos.Features.Pessoa.Models;
+
+namespace ProjetoComFundamentos.Features.Pessoa.Controllers;
+
+public partial class PessoaController
+{
+    private readonly IPesquisaDeCidadePorCepService _pesquisaDeCidadePorCepService;
+
+    public PessoaController(IPesquisaDeCidadePorCepService pesquisaDeCidadePorCepService) 
+        => _pesquisaDeCidadePorCepService = pesquisaDeCidadePorCepService;
+
+    public void AtribuirCidadeNaPessoaBuscandoPeloCep(string cep, PessoaModel pessoaModel) 
+        => pessoaModel.Endereco = _pesquisaDeCidadePorCepService.RetornarEnderecoPeloCep(cep);
+}
